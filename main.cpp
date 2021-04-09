7f6d51d2a1e68bd6e8536baeb7eeb28de03efa7c
d-i-m-a <ivankin00@list.ru>
2021-04-09 17:09:06 +0700

    разделил изначальный код по файлам + .gitignore

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <QCoreApplication>
+
+#include <iostream>
+
+#include "unit.h"
+
+std::string generateProgram() {
+    ClassUnit myClass( "MyClass" );
+
+    myClass.add(std::make_shared<MethodUnit> ("testFunc1", "void", 0), ClassUnit::PUBLIC);
+
+    myClass.add(std::make_shared<MethodUnit> ("testFunc2", "void", MethodUnit::STATIC), ClassUnit::PRIVATE);
+
+    myClass.add(std::make_shared<MethodUnit> ("testFunc3", "void", MethodUnit::VIRTUAL | MethodUnit::CONST), ClassUnit::PUBLIC);
+
+    auto method = std::make_shared<MethodUnit> ("testFunc4", "void", MethodUnit::STATIC);
+
+    method->add(std::make_shared<PrintOperatorUnit> ( R"(Hello,world!\n)" ) );
+
+    myClass.add(method, ClassUnit::PROTECTED );
+
+    return myClass.compile();
+}
+
+//----------------------------------------------------------------------------------------------------
+int main() {
+ std::cout << generateProgram() << std::endl;
+ return 0;
+}
+
